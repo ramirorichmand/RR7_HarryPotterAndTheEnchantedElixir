11d6ee4383ee988e8255aa59fc8ac5e1732587f9
Ramiro <rrcolumbia21@gmail.com>
2023-05-06 19:37:36 +0100

    ingredient persistence

--- src/main/java/com/harrypotterthingy/demo/model/Ingredient.java
+++ src/main/java/com/harrypotterthingy/demo/model/Ingredient.java
@@ -1,4 +1,30 @@
 package com.harrypotterthingy.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
 public class Ingredient {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
 }
