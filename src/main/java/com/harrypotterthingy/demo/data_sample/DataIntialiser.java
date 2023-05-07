1ad51b6212d2754dda1aea752b47c8dad650e93d
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 11:11:17 +0100

    data sample

---
+++ src/main/java/com/harrypotterthingy/demo/data_sample/DataIntialiser.java
@@ -0,0 +1,11 @@
+package com.harrypotterthingy.demo.data_sample;
+
+
+import com.harrypotterthingy.demo.model.*;
+import com.harrypotterthingy.demo.repository.IngredientRepository;
+import com.harrypotterthingy.demo.repository.PotionRepository;
+import com.harrypotterthingy.demo.repository.RecipeRepository;
+import com.harrypotterthingy.demo.repository.StudentRepository;
+
+public class DataIntialiser {
+}
