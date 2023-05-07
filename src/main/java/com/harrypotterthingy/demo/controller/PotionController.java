2242f713982d79cad71960364664fa029dda7fba
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 11:01:52 +0100

    potion controller half fixed

--- src/main/java/com/harrypotterthingy/demo/controller/PotionController.java
+++ src/main/java/com/harrypotterthingy/demo/controller/PotionController.java
@@ -1,4 +1,47 @@
 package com.harrypotterthingy.demo.controller;
 
+package com.harrypotterthingy.demo.controller.exception.PotionNotFound;
+package com.harrypotterthingy.demo.controller.exception.StudentNotFound;
+package com.harrypotterthingy.demo.model.Ingredient;
+import com.harrypotterthingy.demo.model.Potion;
+import com.harrypotterthingy.demo.model.Recipe;
+import com.harrypotterthingy.demo.service.PotionService;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/potions")
 public class PotionController {
+    private final PotionService potionService;
+
+    public PotionController(PotionService potionService) {
+        this.potionService = potionService;
+    }
+
+    @GetMapping
+    public List<Potion> getAllPotions(){
+        return potionService.getAllPotions();
+    }
+
+    @PostMapping("/brew/{potionId}")
+    public void brewPotion(@PathVariable Long potionId) throws StudentNotFound, PotionNotFound {
+        potionService.brewPotion(potionId);
+    }
+
+    @GetMapping("/brew/{studentId}")
+    public List<Potion> getStudentsPotions(@PathVariable Long studentId) throws StudentNotFound {
+        return potionService.getStudentsPotions(studentId);
+    }
+
+    @PutMapping("/{potionId}/add")
+    public Potion addIngredientToPotion(@PathVariable Long potionId, @RequestBody Ingredient ingredient) throws PotionNotFound, StudentNotFound {
+        return potionService.addIngredientToPotion(potionId, Ingredient);
+    }
+
+    @GetMapping("/{potionId}/help")
+    public List<Recipe> getRecipesForPotion(@PathVariable Long potionId) throws PotionNotFound {
+        return potionService.getRecipesForPotion(potionId);
+    }
 }
