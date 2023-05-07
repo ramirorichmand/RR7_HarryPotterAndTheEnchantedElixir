f5976be50585d0f375e3d6bcb4f924b1d95231b9
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:33:53 +0100

    something weird happened

--- src/main/java/com/harrypotterthingy/demo/controller/exception/PotionNotFound.java
+++ src/main/java/com/harrypotterthingy/demo/controller/exception/PotionNotFound.java
@@ -4,8 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class RoomNotFound extends Exception {
-    public RoomNotFound(String message) {
-        super(message);
+public class PotionNotFound extends Exception {
+    public PotionNotFound(String message) { super(message);
     }
 }
