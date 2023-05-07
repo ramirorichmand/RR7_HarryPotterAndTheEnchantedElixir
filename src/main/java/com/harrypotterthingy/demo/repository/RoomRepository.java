57a2ff6ef739e566fc964867d530b183ea4ce0ae
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:50:15 +0100

    room repository

--- src/main/java/com/harrypotterthingy/demo/repository/RoomRepository.java
+++ src/main/java/com/harrypotterthingy/demo/repository/RoomRepository.java
@@ -1,6 +1,10 @@
 package com.harrypotterthingy.demo.repository;
 
 
+import com.harrypotterthingy.demo.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 }
