2971239146fe8e45f180cc296431c85d2f0c8738
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:57:03 +0100

    model ingredient done, room controller related

--- src/main/java/com/harrypotterthingy/demo/model/Ingredient.java
+++ src/main/java/com/harrypotterthingy/demo/model/Ingredient.java
@@ -8,9 +8,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
