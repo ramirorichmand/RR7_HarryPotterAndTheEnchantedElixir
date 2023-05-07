5d1195dafc87c0c8626f462a26c3ed88f3475c12
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:51:42 +0100

    potion repository

--- src/main/java/com/harrypotterthingy/demo/repository/PotionRepository.java
+++ src/main/java/com/harrypotterthingy/demo/repository/PotionRepository.java
@@ -1,4 +1,13 @@
 package com.harrypotterthingy.demo.repository;
 
-public class PotionRepository {
+import com.harrypotterthingy.demo.model.Potion;
+import com.harrypotterthingy.demo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PotionRepository extends JpaRepository<Potion, Long> {
+    List<Potion> getPotionByStudent (Student student);
 }
