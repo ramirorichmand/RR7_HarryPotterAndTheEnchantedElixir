44dda15737ca92e65b9954ea6d75eb628d84bda7
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:28:09 +0100

    potion not found exception

--- src/main/java/com/harrypotterthingy/demo/controller/exception/PotionNotFound.java
+++ src/main/java/com/harrypotterthingy/demo/controller/exception/PotionNotFound.java
@@ -1,4 +1,10 @@
 package com.harrypotterthingy.demo.controller.exception;
 
-public class PotionNotFound {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PotionNotFound extends Exception {
+    public PotionNotFound(String message) { super(message);
+    }
 }
