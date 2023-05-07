f0cb5ff3c2ba61bb595668f26784bfa33af1ee9c
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 11:12:23 +0100

    first part done

--- src/main/java/com/harrypotterthingy/demo/data_sample/DataIntialiser.java
+++ src/main/java/com/harrypotterthingy/demo/data_sample/DataIntialiser.java
@@ -6,6 +6,9 @@ import com.harrypotterthingy.demo.repository.IngredientRepository;
 import com.harrypotterthingy.demo.repository.PotionRepository;
 import com.harrypotterthingy.demo.repository.RecipeRepository;
 import com.harrypotterthingy.demo.repository.StudentRepository;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 public class DataIntialiser {
 }
