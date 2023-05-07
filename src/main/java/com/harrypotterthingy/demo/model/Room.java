a3eddda14785c31278e3cab49de06b03ce342346
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:54:34 +0100

    model room fixed

--- src/main/java/com/harrypotterthingy/demo/model/Room.java
+++ src/main/java/com/harrypotterthingy/demo/model/Room.java
@@ -1,13 +1,11 @@
 package com.harrypotterthingy.demo.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.*;
 
-import javax.persistence.*;
 import java.util.Set;
 
 @Getter
