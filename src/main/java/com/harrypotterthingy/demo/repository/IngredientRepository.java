04b6dcad0852893271b56fad317c827cadd1e6ba
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:52:14 +0100

    ingredient repository

--- src/main/java/com/harrypotterthingy/demo/repository/IngredientRepository.java
+++ src/main/java/com/harrypotterthingy/demo/repository/IngredientRepository.java
@@ -1,4 +1,9 @@
 package com.harrypotterthingy.demo.repository;
 
-public class IngredientRepository {
+import com.harrypotterthingy.demo.model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 }
