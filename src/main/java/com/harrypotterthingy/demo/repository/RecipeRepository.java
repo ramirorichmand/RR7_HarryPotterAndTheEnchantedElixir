c473f0758d020011c75a79158535caf8bbd32d81
Ramiro <rrcolumbia21@gmail.com>
2023-05-07 10:51:04 +0100

    recipe repository

--- src/main/java/com/harrypotterthingy/demo/repository/RecipeRepository.java
+++ src/main/java/com/harrypotterthingy/demo/repository/RecipeRepository.java
@@ -1,4 +1,9 @@
 package com.harrypotterthingy.demo.repository;
 
-public class RecipeRepository {
+import com.harrypotterthingy.demo.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }
